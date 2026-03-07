58b28bb82b14aab858a49dfa021b819813df8c79
hewertonmarques <hewertonrb@yahoo.com.br>
2026-03-06 23:00:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="dash.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="dashboard-container">
+    <nav>
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Relatórios</a></li>
+            <li><a href="#">Configurações</a></li>
+        </ul>
+        <div class="perfil-usuario">
+            <img src="https:ui-avatars.com/api/?name=Hewerton+Marques&background=000000&color=fff" alt="Avatar">
+            <span>Hewerton</span>
+        </div>
+    </nav>
+
+    <main>
+        <section class="container-cards">
+
+        <article>
+            <h2>Módulo de Usuários</h2>
+            <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+            <a href="#" class="btn">Acessar</a>
+        </article>
+            <article>
+            <h2>Acesso de Usuários</h2>
+            <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+            <a href="#" class="btn">Acessar</a>
+        </article>
+         <article>
+            <h2>Ajuda a Usuários</h2>
+            <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+            <a href="#" class="btn">Acessar</a>
+        </article>
+        </section>
+     </main>
+</div>
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> - Desenvolvido na aula de Web I</p>
+</footer>
+</body>
+</html>
